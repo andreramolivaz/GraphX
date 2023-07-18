ac3b24e3acfcade9a83ad731edd3ee05e82910c9
andreramolivaz <92636448+andreramolivaz@users.noreply.github.com>
2023-07-18 22:59:26 +0200

    implemented the main and most basic functionality

--- GraphX/src/SimpleGraph.java
+++ GraphX/src/SimpleGraph.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class SimpleGraph<T> extends Graph{
+public class SimpleGraph implements Graph {
     private List<UndirectedEdge> edges;
     private List<Node> nodes_isolated;
 
@@ -42,8 +43,68 @@ public class SimpleGraph<T> extends Graph{
         UndirectedEdge e = new UndirectedEdge(A, B);
         return edges.contains(e);
     }
+    public boolean hasNode(Node node){
+        for (UndirectedEdge e : edges)
+              if (e.getNodes().first.equals(node) || e.getNodes().second.equals(node)) return true;
+        return nodes_isolated.contains(node);
+    }
+    public int getEdgeCount(){
+        return edges.size();
+    }
+    public int getNodeCount(){
+        int res = 0;
+        for (UndirectedEdge e : edges){
+            Pair<Node, Node> nodes = e.getNodes();
+            if (!nodes_isolated.contains(nodes.first)) res++;
+            if (!nodes_isolated.contains(nodes.second)) res++;
+        }
+        return res + nodes_isolated.size();
+    }
+    public List<Edge> getEdges(){
+        return new ArrayList<Edge>(edges);
+    }
+    public List<Node> getNodes(){
+         List<Node> result = new ArrayList<Node>(nodes_isolated);
+         for (UndirectedEdge e : edges){
+             Pair<Node, Node> nodes = e.getNodes();
+             if (!result.contains(nodes.first)) result.add(nodes.first);
+             if (!result.contains(nodes.second)) result.add(nodes.second);
+         }
+         return result;
+    }
 
-
+    public boolean contains(Graph g){
+        if (g == null) return false;
+        if (!(g instanceof SimpleGraph)) return false;
+        SimpleGraph sg = (SimpleGraph) g;
+        for (Object e : sg.getEdges())
+              if (!edges.contains(e)) return false;
+        return true;
+    }
+    public Graph getShortestPath(Node n1, Node n2){
+        return null;
+    }
+    public Graph getWeightedShortestPath(Node A, Node B) throws GraphNotWeightedException{
+        return null;
+    }
+    public boolean isAcyclic(){
+        return false;
+    }
+public Graph getUnion(Graph g){
+        return null;
+    }
+    public Graph getIntersection(Graph g){
+        return null;
+    }
+    public Iterator<Node> dfsIterator(Node start){
+        return null;
+    }
+    public Iterator<Node> bfsIterator(Node start){
+        return null;
+    }
+    public Iterator<Node> iterator(){
+        return null;
+    }
 
     @Override
     public boolean equals (Object o){
